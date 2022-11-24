d3b4a5cd1944625ea96c56e184fe850ab7a96c6c
epifanovgv.24 <glegogo@yandex.ru>
2022-11-24 12:54:09 +0300

    classes

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -5,4 +5,8 @@ import io.github.humbleui.jwm.Event;
 import java.util.function.Consumer;
 
 public class Application implements Consumer<Event> {
+    @Override
+    public void accept(Event event) {
+
+    }
 }
