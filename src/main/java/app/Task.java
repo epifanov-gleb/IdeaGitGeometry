79ea96abf3b1cba933e781d1daaa69400fa43111
epifanovgv.24 <glegogo@yandex.ru>
2023-01-26 12:36:19 +0300

    solve button

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -93,23 +93,40 @@ public class Task {
     protected CoordinateSystem2i lastWindowCS;
 
     /**
+     * Флаг, решена ли задача
+     */
+    private boolean solved;
+
+    /**
      * Очистить задачу
      */
     public void clear() {
         points.clear();
+        solved = false;
     }
 
     /**
      * Решить задачу
      */
     public void solve() {
+        solved = true;
         PanelLog.warning("Вызван метод solve()\n Пока что решения нет");
     }
+
     /**
      * Отмена решения задачи
      */
     public void cancel() {
+        solved = false;
+    }
 
+    /**
+     * проверка, решена ли задача
+     *
+     * @return флаг
+     */
+    public boolean isSolved() {
+        return solved;
     }
 
     /**
@@ -138,9 +155,9 @@ public class Task {
      * @param pointSet множество
      */
     public void addPoint(Vector2d pos, Point.PointSet pointSet) {
+        solved = false;
         Point newPoint = new Point(pos, pointSet);
         points.add(newPoint);
-        // Добавляем в лог запись информации
         PanelLog.info("точка " + newPoint + " добавлена в " + newPoint.getSetName());
     }
 
