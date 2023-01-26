541701f1f47ce362b2390e48002d539b19064820
epifanovgv.24 <glegogo@yandex.ru>
2023-01-26 12:54:52 +0300

    solve demo

--- src/main/java/app/Colors.java
+++ src/main/java/app/Colors.java
@@ -40,4 +40,12 @@ public class Colors {
      * Цвет текста
      */
     public static final int FIELD_TEXT_COLOR = Misc.getColor(255, 0, 0, 0);
+    /**
+     * Цвет пересечения
+     */
+    public static final int CROSSED_COLOR = Misc.getColor(200, 0, 255, 255);
+    /**
+     * Цвет разности
+     */
+    public static final int SUBTRACTED_COLOR = Misc.getColor(200, 255, 255, 0);
 }
